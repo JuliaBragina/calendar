e9db1d2cd47e3f240651d6cb1137c000c613332f
Julia Bragina <triulya@yandex.ru>
2024-06-07 15:07:30 +0000

    изменена маршрутизация

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Импортируем HashRouter
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from '../src/components/App';
 import styled from 'styled-components';
@@ -24,13 +24,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RootContainer>
     <React.StrictMode>
-      <HashRouter basename="/calendar"> {/* Устанавливаем basename для HashRouter */}
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   </RootContainer>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
