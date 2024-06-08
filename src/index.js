198de1e3458b11edf9022c0cf06ffec71267b7aa
Julia Bragina <triulya@yandex.ru>
2024-06-08 20:46:52 +0000

    изменена маршрутизация

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from '../src/components/App';
 import styled from 'styled-components';
@@ -24,9 +24,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RootContainer>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </RootContainer>
 );
